f854ff0cfd6bc4fe3eade4cfbc1d9134ed77c13f
GeorgeCheng(Laptop) <ck40292@gmail.com>
2023-05-30 16:52:06 +0800

    init: header

---
+++ pages/editTeam/index.php
@@ -0,0 +1,14 @@
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>鄭翔駿</title>
+    <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body id="wrapper">
+    <?php include "header\index.php"; ?>
+    <inpu type="text" name="">
+</body>
+
+</html>
